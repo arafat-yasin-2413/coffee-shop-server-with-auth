c64368398dacf6eb79d055d8696a2b31c3fde8b8
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-05-18 12:39:25 +0600

    56-1 Done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// root api
+app.get('/', (req, res)=>{
+    res.send('Coffee server is getting hotter');
+});
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Coffee server is running on port : ${port}`);
+})
